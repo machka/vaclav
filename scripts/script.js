c6c7b92dc7cdf7928692a4597ca11bbfcc5605a6
Václav Machka <vaclav.machka@gmail.com>
2016-09-25 20:36:09 +0200

    New pages

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,28 @@ var app = angular.module('vaclav-app', ['ngRoute']);
 app.config(function($routeProvider){
       $routeProvider
           .when('/',{
-                templateUrl: 'home.html'
+                templateUrl: 'pages/onas.html'
+          })
+          .when('/onas',{
+                templateUrl: 'pages/onas.html'
           })
           .when('/servis',{
                 templateUrl: 'pages/servis.html'
+          })
+          .when('/bazar',{
+                templateUrl: 'pages/bazar.html'
+          })
+          .when('/kontakt',{
+                templateUrl: 'pages/kontakt.html'
           });
+});
+
+$( document ).ready(function() {
+      $("ul.nav-pills a").on('click', function(){
+          $("ul.nav-pills li").each(function(){
+                $(this).removeClass('active');
+          })
+
+          $(this).parent().addClass('active');
+      })
 });

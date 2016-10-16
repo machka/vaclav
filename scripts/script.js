7d2731fe471756ba1c8c83ec4a587bd9eff0741d
Václav Machka <vaclav.machka@gmail.com>
2016-10-16 18:28:43 +0200

    Remove angular, add router.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,47 @@
-var app = angular.module('vaclav-app', ['ngRoute']);
-
-app.config(function($routeProvider){
-      $routeProvider
-          .when('/',{
-                templateUrl: 'pages/onas.html'
-          })
-          .when('/onas',{
-                templateUrl: 'pages/onas.html'
-          })
-          .when('/servis',{
-                templateUrl: 'pages/servis.html'
-          })
-          .when('/bazar',{
-                templateUrl: 'pages/bazar.html'
-          })
-          .when('/kontakt',{
-                templateUrl: 'pages/kontakt.html'
-          });
-});
-
 $(document).ready(function() {
-      $("ul.nav a").on('click', function(){
-          $("ul.nav li").each(function(){
-                $(this).removeClass('active');
-          })
-
-          $(this).parent().addClass('active');
+      var router = new Router()
+      .addRoute('#/', function(req, next){
+            $("#tab-onas").show();
+            $("#li-onas").addClass('active');
       })
+      .addRoute('#/onas', function(req, next){
+            $("#tab-onas").show();
+            $("#li-onas").addClass('active');
+      })
+      .addRoute('#/servis', function(req, next){
+            $("#tab-servis").show();
+            $("#li-servis").addClass('active');
+      })
+      .addRoute('#/bazar', function(req, next){
+            $("#tab-bazar").show();
+            $("#li-bazar").addClass('active');
+      })
+      .addRoute('#/kontakt', function(req, next){
+            $("#tab-kontakt").show();
+            $("#li-kontakt").addClass('active');
+
+            var stred = SMap.Coords.fromWGS84(14.3771451, 50.1341075);
+            var mapa = new SMap(JAK.gel("mapa"), stred, 17);
+            mapa.addDefaultLayer(SMap.DEF_BASE).enable();
+            mapa.addDefaultControls();
+
+            var layer = new SMap.Layer.Marker();
+            mapa.addLayer(layer);
+            layer.enable();
+
+            var options = {};
+            var marker = new SMap.Marker(stred, "myMarker", options);
+            layer.addMarker(marker);
+      })
+      .before(function(req,next){
+            $("#tab-onas").hide();
+            $("#tab-servis").hide();
+            $("#tab-bazar").hide();
+            $("#tab-kontakt").hide();
+            $("ul.nav li").each(function(){
+                $(this).removeClass('active');
+            })
+            next();
+      })
+      .run();
 });
